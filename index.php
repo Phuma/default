7ead8b1cc31f5ed7a101bff4c998e70849a7ac69
Phuma <mark@marksoerensen.dk>
2015-06-14 00:12:49 +0200

    Add jq and bs

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<p>Mark World</p>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Mark Philip Alexander Sørensen</title>
+		<link rel="stylesheet" href="style.less" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" />
+	</head>
+	
+	<body>
+		<p>Mark</p>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</body>
+</html>
