ad9aa3e832bd01191cea277f467b75a6d3cc3608
Phuma <mark@marksoerensen.dk>
2015-06-14 00:25:39 +0200

    header, article, footer added

--- index.php
+++ index.php
@@ -7,8 +7,18 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" />
 	</head>
 	
-	<body>
-		<p>Mark</p>
+	<body class="row">
+		<header class="col-sm-12">
+		</header>
+		<article class="col-sm-12">
+			<section class="col-sm-3"></section>
+			<section class="col-sm-3"></section>
+			<section class="col-sm-3"></section>
+			<section class="col-sm-3"></section>
+		</article>
+		<footer>
+			
+		</footer>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	</body>
