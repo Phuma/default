8b128cc3f72660191d13e8962acaf829370f92eb
Phuma <mark@marksoerensen.dk>
2015-06-21 23:04:55 +0200

    added email.php

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	$email_from = $_POST['email'];
+    $email_subject = $_POST['subject'];
+    $email_body = $_POST['msg'];
+	
+	$to = "mark@marksoerensen.dk";
+	
+	$headers = "From: $email_from";
+	
+	//mail($to,$email_subject,$email_body,$headers);
+	
+?>
