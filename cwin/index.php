4106742b8995acbc2a1496d0665325431b2788b7
Phuma <mark@marksoerensen.dk>
2015-09-20 22:15:37 +0200

    start project cwin

---
+++ cwin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<title>Mark Philip Alexander Sørensen</title>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" />
+		<link rel="stylesheet/less" type="text/css" href="style/style.less" />
+	</head>
+	
+	<body class="row">
+		<section>
+			Hello world
+		</section>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript">></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript">></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js" type="text/javascript">></script>
+	</body>
+</html>
+	
