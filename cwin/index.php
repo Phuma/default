6e6150fa9e374574319e9292fc7b28db42f1687b
Phuma <mark@marksoerensen.dk>
2015-11-01 23:28:43 +0100

    updates

--- cwin/index.php
+++ cwin/index.php
@@ -3,18 +3,136 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		<title>Mark Philip Alexander Sørensen</title>
+		<title>Friends of CWIN Denmark</title>
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" />
 		<link rel="stylesheet/less" type="text/css" href="style/style.less" />
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,300,100' rel='stylesheet' type='text/css'>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js" type="text/javascript">></script>
 	</head>
 	
-	<body class="row">
-		<section>
-			Hello world
+	<body class="container-fluid">
+		<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '492023407626119',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+		<section class="fullscreenimage">
+			<div class="logo">
+				<img src="image/friends_of_cwin.jpg" title="friends of cwin denmark" alt="friends of cwin denmark"/>
+			</div>
+			<div class="anchor">
+				
+			</div>
 		</section>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript">></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript">></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js" type="text/javascript">></script>
+		<section class="row">
+			<section class="text container section-one">
+				<div class="col-sm-6 col-xs-12 paragraf">
+					<h1>Vores Arbejde</h1>	
+					<p>Friends of CWIN, Denmark er en venskabsforening til Child Workers in Nepal (CWIN).</p>
+					<p>Vores formål er at støtte CWINs arbejde med børn og børns rettigheder i Nepal. Helt konkret vil vi i foreningen støtte CWIN økonomisk i etableringen af et hjem for drenge i lighed med Peace Home, CWINs hjem for piger.</p>
+					<p>Trafficking er et centralt problem i Nepal, og mange børn bliver handlet på grund af fattigdom. De ender oftest som husslaver, i prostitution, på falske børnehjem, som børnearbejdere eller på gaden.</p>
+					<p>Trafficking er et centralt problem i Nepal, og mange børn bliver handlet på grund af fattigdom.</p>
+					<p>De ender oftest som husslaver, i prostitution, på falske børnehjem, som børnearbejdere eller på gaden.</p>
+				</div>
+				<div class="col-sm-6 col-xs-12 paragraf no-heading">
+					<p>De heldige bliver reddet af organisationer som CWIN.</p>
+					<p>Peace Home er et sted, hvor udsatte piger kan leve sikkert mens de vokser op og går i skole, og hvor de bliver hørt og taget alvorligt.</p>
+					<p>De fleste piger har kontakt til deres familie og er ved at gennemgå en genforeningsproces med nær familie eller andre slægtninge.</p>
+					<p>CWIN har selv identificeret behovet for et lignende tilbud til udsatte drenge, og vi håber at vi kan hjælpe dem med at nå ud til endnu flere udsatte børn i Nepal, særligt efter det store jordskælv d. 25. april 2015, som har gjort tilværelsen usikker for endnu flere børn.</p>
+				</div>
+			</section>
+			<section class="bgimage section-two">
+				<div class="text container">
+					<div class="col-xs-12 no-padding">
+						<h1 class="quote">Worlds most important quote</h1>
+					</div>
+				</div>
+			</section>
+			<section class="text container section-three">
+				<div class="col-sm-4 col-xs-12 paragraf links">
+					<p>Friends of CWIN, Denmark</p>
+					<p>CVR-nummer: <a target="_blank" href="http://datacvr.virk.dk/data/visenhed?enhedstype=virksomhed&id=37046337&soeg=37%2004%2063%2037">37 04 63 37</a></p>
+				</div>
+				<div class="col-sm-4 hidden-xs paragraf links">
+					<p>Formand: Henrik Andersen – tlf <a class="tel" href="tel:+45 2999 3264">+45 2999 3264</a></p>
+					<p>Næstformand: Annette Poulsen – tlf <a class="tel" href="tel:+45 2273 0745">+45 2273 0745</a></p>
+				</div>
+				<div class="hidden-lg hidden-md hidden-sm col-xs-12 paragraf links">
+					<p>Formand: Henrik Andersen</p> 
+					<p>tlf +45 2999 3264</p>
+					<p>Næstformand: Annette Poulsen</p>
+					<p>tlf +45 2273 0745</p>
+				</div>
+				<div class="col-sm-4 col-xs-12 paragraf links">
+					<a href="mailto:fcwin@gmail.com">
+						<svg class="icon icon-envelope"><use xlink:href="#icon-envelope"></use>
+							<symbol id="icon-envelope" viewBox="0 0 1216 1024">
+								<title>envelope</title>
+								<path class="path1" d="M641.92 13.216c-18.56-17.056-49.248-17.056-67.808 0l-566.016 341.472c-4.8 2.912-7.744 8.096-7.744 13.696v605.504c0 27.616 22.592 50.112 50.368 50.112h1114.56c27.776 0 50.368-22.496 50.368-50.112v-605.504c0-5.6-2.944-10.784-7.744-13.696l-565.984-341.472zM592.128 39.68c1.12-0.672 2.112-1.472 3.040-2.368 3.456-3.424 8-5.312 12.832-5.312s9.376 1.888 12.8 5.312c0.928 0.896 1.952 1.696 3.040 2.368l551.264 332.608-399.296 237.952c-7.584 4.544-10.080 14.336-5.568 21.952 3.008 5.024 8.32 7.808 13.76 7.808 2.784 0 5.6-0.704 8.16-2.24l391.84-233.28v568.224l-541.92-360.512c-17.216-13.728-51.008-13.728-67.072-0.8l-543.008 361.152v-568.064l391.808 233.28c2.56 1.536 5.408 2.24 8.192 2.24 5.44 0 10.752-2.784 13.76-7.808 4.512-7.584 2.016-17.408-5.568-21.952l-399.328-237.952 551.264-332.608zM1154.88 992h-1094.016l532.992-354.752c5.536-4.416 22.784-4.384 29.408 0.8l531.616 353.952zM208 416h800c8.832 0 16-7.168 16-16s-7.168-16-16-16h-800c-8.832 0-16 7.168-16 16s7.168 16 16 16z"></path>
+							</symbol>
+						</svg>
+					</a>
+				</div>
+			</section>
+		</section>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript">></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript">></script>
+		
+		<script>
+			$( document ).ready(function() {
+				$('.fullscreenimage').css({
+					'height': $(window).height(),
+					'width': $(window).width()
+				})
+				$('.fullscreenimage, .logo').css('opacity', '1');
+				
+				$('.anchor').click(function(){
+					if($(window).width() < 768) {
+						$('html, body').animate({
+				        scrollTop: $('.section-one').position().top
+				    	}, 1500);
+					}
+					else {
+						$('html, body').animate({
+				        scrollTop: $('.section-one').position().top + ($('.section-one').height()/2) - ($(window).height()/2)
+				    	}, 1500);
+					}
+				});
+			    
+			    $(window).scroll(function() {
+					$('.text').each(function(){
+					var imagePos = $(this).offset().top;
+
+					var topOfWindow = $(window).scrollTop();
+					var scrollBottom = $(window).scrollTop() + $(window).height() - 100;
+						if (imagePos < scrollBottom) {
+							$(this).css('opacity', '1');
+						}
+					});
+				});
+				// Facebook stuff
+				
+				
+			});
+		</script>
 	</body>
 </html>
 	
